140b509e75df8319d3b47957c8a4eb85e239a328
matvey144 <matveybb@yandex.ru>
2025-11-01 17:49:51 +0300

    Now sections of the board are drawn.

--- main.c
+++ main.c
@@ -19,17 +19,35 @@ int main(void)
 			DrawRectangleLinesEx((Rectangle){(float)WINDOW_H / 2, 0.0, (float)WINDOW_H / 2, (float)WINDOW_H},
 					PADDING, (Color) {0x6F, 0x4F, 0x2F, 0xFF});
 			// Board markings
-			const float trianglePad = (WINDOW_H / 2.0 - PADDING * 2.0) / 24.0;
-			// Top
+			const float trianglePad = (WINDOW_H / 2.0 - PADDING * 2.0) / 19.0;
+			// Left
 			for (int i = 0; i < 6; i++) {
 				float leftmostX = PADDING + trianglePad * 3.0 * i + trianglePad;
-				DrawTriangle((Vector2) {leftmostX, PADDING},
-						(Vector2) {leftmostX + trianglePad * 2, PADDING},
-						(Vector2) {leftmostX + trianglePad, PADDING},
-						(Color) {0x55, 0x31, 0x11, 0xFF});
+				// Top
+				Vector2 v1 = {leftmostX, PADDING};
+				Vector2 v2 = {leftmostX + trianglePad, (WINDOW_H / 2) - PADDING};
+				Vector2 v3 = {leftmostX + trianglePad * 2, PADDING};
+				DrawTriangle(v1, v2, v3, (Color) {0x55, 0x31, 0x11, 0xFF});
+				// Bottom
+				v1.y = WINDOW_H - PADDING;
+				v2.y = WINDOW_H / 2.0 + PADDING;
+				v3.y = WINDOW_H - PADDING;
+				DrawTriangle(v1, v3, v2, (Color) {0x55, 0x31, 0x11, 0xFF});
 			}
-			// Bottom
-			for (int i = 0; i < 6; i++) {}
+			// Right
+			for (int i = 0; i < 6; i++) {
+				float leftmostX = WINDOW_H / 2.0 + PADDING + trianglePad * 3.0 * i + trianglePad;
+				// Top
+				Vector2 v1 = {leftmostX, PADDING};
+				Vector2 v2 = {leftmostX + trianglePad, (WINDOW_H / 2) - PADDING};
+				Vector2 v3 = {leftmostX + trianglePad * 2, PADDING};
+				DrawTriangle(v1, v2, v3, (Color) {0x55, 0x31, 0x11, 0xFF});
+				// Bottom
+				v1.y = WINDOW_H - PADDING;
+				v2.y = WINDOW_H / 2.0 + PADDING;
+				v3.y = WINDOW_H - PADDING;
+				DrawTriangle(v1, v3, v2, (Color) {0x55, 0x31, 0x11, 0xFF});
+			}
 		}
 		EndDrawing();
 	}
