82ddc33c9ae3e26460b6ee866e5e1f1749813921
matvey144 <matveybb@yandex.ru>
2025-11-04 11:45:11 +0300

    Now piece display system is fully functional

--- main.c
+++ main.c
@@ -30,6 +30,7 @@ int main(void)
 					PADDING, (Color) {0x6F, 0x4F, 0x2F, 0xFF});
 			// Board markings
 			const float trianglePad = (WINDOW_H / 2.0 - PADDING * 2.0) / 19.0;
+			const float pieceRadius = trianglePad * 0.60;
 			// Left
 			for (int i = 0; i < 6; i++) {
 				float leftmostX = PADDING + trianglePad * 3.0 * i + trianglePad;
@@ -41,10 +42,10 @@ int main(void)
 				// Drawing pieces
 				if (board[i] != 0) {
 					Color pieceColor = board[i] > 0 ? WHITE : BLACK;
-					for (int j = 0; j < abs(board[i]); j++){
-						DrawCircle(leftmostX + trianglePad,
-								PADDING + trianglePad + trianglePad * 2 * j,
-								trianglePad, pieceColor);
+					for (int j = 0; j < abs(board[i]); j++) {
+						int x = leftmostX + trianglePad;
+						int y = PADDING + pieceRadius + pieceRadius * 2 * j;
+						DrawCircle(x, y, pieceRadius, pieceColor);
 					}
 				}
 				// Bottom
@@ -52,13 +53,15 @@ int main(void)
 				v2.y = WINDOW_H / 2.0 + PADDING;
 				v3.y = WINDOW_H - PADDING;
 				DrawTriangle(v1, v3, v2, (Color) {0x55, 0x31, 0x11, 0xFF});
-				if (board[i+12] != 0) {
-					int temp = i + 12;
+				// Drawing pieces
+				if (board[23-i] != 0) {
+					int temp = 23 - i;
 					Color pieceColor = board[temp] > 0 ? WHITE : BLACK;
-					for (int j = 0; j < abs(board[temp]); j++)
-						DrawCircle(leftmostX + trianglePad,
-								WINDOW_H - PADDING - trianglePad - trianglePad * 2 * j,
-								trianglePad, pieceColor);
+					for (int j = 0; j < abs(board[temp]); j++) {
+						int x = leftmostX + trianglePad;
+						int y = WINDOW_H - PADDING - pieceRadius - pieceRadius * 2 * j;
+						DrawCircle(x, y, pieceRadius, pieceColor);
+					}
 				}
 			}
 			// Right
@@ -69,12 +72,33 @@ int main(void)
 				Vector2 v2 = {leftmostX + trianglePad, (WINDOW_H / 2) - PADDING};
 				Vector2 v3 = {leftmostX + trianglePad * 2, PADDING};
 				DrawTriangle(v1, v2, v3, (Color) {0x55, 0x31, 0x11, 0xFF});
+				// Drawing pieces
+				if (board[i+6] != 0) {
+					int temp = i + 6;
+					Color pieceColor = board[temp] > 0 ? WHITE : BLACK;
+					for (int j = 0; j < abs(board[temp]); j++) {
+						int x = leftmostX + trianglePad;
+						int y = PADDING + pieceRadius + pieceRadius * 2 * j;
+						DrawCircle(x, y, pieceRadius, pieceColor);
+					}
+				}
 				// Bottom
 				v1.y = WINDOW_H - PADDING;
 				v2.y = WINDOW_H / 2.0 + PADDING;
 				v3.y = WINDOW_H - PADDING;
 				DrawTriangle(v1, v3, v2, (Color) {0x55, 0x31, 0x11, 0xFF});
+				// Drawing pieces
+				if (board[17-i] != 0) {
+					int temp = 17 - i;
+					Color pieceColor = board[temp] > 0 ? WHITE : BLACK;
+					for (int j = 0; j < abs(board[temp]); j++) {
+						int x = leftmostX + trianglePad;
+						int y = WINDOW_H - PADDING - pieceRadius - pieceRadius * 2 * j;
+						DrawCircle(x, y, pieceRadius, pieceColor);
+					}
+				}
 			}
+
 			// Side bar
 			DrawRectangle(WINDOW_H, 0, WINDOW_W - WINDOW_H, WINDOW_H, GRAY);
 			int titlePad = (WINDOW_W - WINDOW_H - MeasureText("Nardy", 32)) / 2;
